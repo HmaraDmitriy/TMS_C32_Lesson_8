e73c37ef31c1b7d94bcb5de3ce4c25a0bd9cd6e2
HmaraDmitriy <firmasa191@gmail.com>
2024-10-29 12:05:12 +0300

    Removed interfaces in MasterCard and VisaCard

--- src/src/com/teachmeskills/lesson_8/model/card/MasterCard.java
+++ src/src/com/teachmeskills/lesson_8/model/card/MasterCard.java
@@ -1,13 +1,10 @@
 package com.teachmeskills.lesson_8.model.card;
 
-import com.teachmeskills.lesson_8.model.account.Account;
-import com.teachmeskills.lesson_8.model.document.Check;
-import com.teachmeskills.lesson_8.transfer.CardTransferService;
 import com.teachmeskills.lesson_8.utils.Constants;
 
 import java.util.Date;
 
-public class MasterCard extends BaseCard implements CardTransferService, Constants {
+public class MasterCard extends BaseCard implements Constants {
 
     public String country;
     public int limit;
@@ -22,31 +19,5 @@ public class MasterCard extends BaseCard implements CardTransferService, Constan
     @Override
     public boolean checkCardLimitTransfer(double sum) {
         return sum <= MAX_MASTERCARD_TRANSFER_LIMIT && this.amount >= sum;
-    }
-
-    @Override
-    public Check transferFromCardToCard(BaseCard senderCard, BaseCard receiverCard, double sum, int data, int numberCart) {
-        if (checkCardLimitTransfer(sum) && senderCard.amount >= sum) {
-            senderCard.amount -= sum;
-            receiverCard.amount += sum;
-            System.out.println("Transfer completed successfully.");
-            return new Check(sum, data, numberCart);
-        } else {
-            System.out.println("Transfer failed.");
-            return new Check(sum, data, numberCart);
-        }
-    }
-
-    @Override
-    public Check transferFromCardToAccount(BaseCard senderCard, Account receiverAccount, double sum, int data, int numberCart) {
-        if (checkCardLimitTransfer(sum) && senderCard.amount >= sum) {
-            senderCard.amount -= sum;
-            receiverAccount.setBalance(receiverAccount.getBalance() + sum); // Зачисление суммы на счет получателя
-            System.out.println("Transfer completed successfully.");
-            return new Check(sum, data, numberCart);
-        } else {
-            System.out.println("Transfer faileds.");
-            return new Check(sum, data, numberCart);
-        }
     }
 }
--- src/src/com/teachmeskills/lesson_8/model/card/VisaCard.java
+++ src/src/com/teachmeskills/lesson_8/model/card/VisaCard.java
@@ -1,13 +1,10 @@
 package com.teachmeskills.lesson_8.model.card;
 
-import com.teachmeskills.lesson_8.model.account.Account;
-import com.teachmeskills.lesson_8.model.document.Check;
-import com.teachmeskills.lesson_8.transfer.CardTransferService;
 import com.teachmeskills.lesson_8.utils.Constants;
 
 import java.util.Date;
 
-public class VisaCard extends BaseCard implements CardTransferService, Constants {
+public class VisaCard extends BaseCard implements Constants {
 
     int cashback;
 
@@ -21,32 +18,6 @@ public class VisaCard extends BaseCard implements CardTransferService, Constants
     @Override
     public boolean checkCardLimitTransfer(double sum) {
         return sum <= MAX_VISA_TRANSFER_LIMIT && this.amount >= sum;
-    }
-
-    @Override
-    public  Check transferFromCardToCard(BaseCard senderCard, BaseCard receiverCard, double sum, int data, int numberCart) {
-        if (checkCardLimitTransfer(sum) && this.amount >= sum) {
-            this.amount -= sum;
-            receiverCard.amount += sum;
-            System.out.println("Transfer completed successfully.");
-            return new Check(sum, data, numberCart);
-        } else {
-            System.out.println("Transfer failed.");
-            return new Check(sum, data, numberCart);
-        }
-    }
-
-    @Override
-    public  Check transferFromCardToAccount(BaseCard senderCard, Account receiverAccount, double sum, int data, int numberCart) {
-        if (checkCardLimitTransfer(sum) && this.amount >= sum) {
-            this.amount -= sum;
-            receiverAccount.setBalance(receiverAccount.getBalance() + sum);
-            System.out.println("Transfer completed successfully.");
-            return new Check(sum, data, numberCart);
-        } else {
-            System.out.println("Transfer failed.");
-            return new Check(sum, data, numberCart);
-        }
     }
 }
 
